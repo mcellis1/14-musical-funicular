19bbf747c6d4d3829afc77f7584e8b4cfb858720
napling <matthewcellis98@gmail.com>
2024-12-12 01:18:43 -0600

    added login logout and signup functionality

---
+++ public/js/logout.js
@@ -0,0 +1,16 @@
+const logoutBtn = document.querySelector('#logout')
+
+const logout = async () => {
+    const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert('failed to logout');
+    }
+};
+
+logoutBtn.addEventListener('click', logout);
