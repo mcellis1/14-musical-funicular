b623f53fb40c649b5b343fab25dac78e4d6a3f00
napling <matthewcellis98@gmail.com>
2024-12-14 12:59:09 -0600

    added delete functionality

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,1 +1,26 @@
-console.log()
+const deletePostHandler = async (event) => {
+    let result = confirm('are you sure you want to delete this blog?');
+
+    if (result) {
+        event.preventDefault();
+        const clicked = event.target
+        const id = clicked.dataset.id
+
+        const response = await fetch(`/api/blogs/${id}`, {
+            method: 'DELETE'
+        });
+        if (response.ok) {
+            document.location.assign(`/dashboard`);
+        } else {
+            alert(response.statusText);
+        }
+    } else {
+        return
+    }
+};
+
+const deleteButton = document.querySelectorAll("#deleteBtn");
+
+for (let i = 0; i < deleteButton.length; i++) {
+    deleteButton[i].addEventListener("click", deletePostHandler);
+}
