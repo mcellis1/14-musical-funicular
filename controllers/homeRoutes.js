12e497a52afbec3ff8abb747f04c621b73c88c5c
napling <matthewcellis98@gmail.com>
2024-12-14 12:39:58 -0600

    added routes for blog and edit pages

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -10,17 +10,17 @@ router.get('/', async (req, res) => {
                 {
                     model: User,
                     attributes: ['username'],
-                },
-                {
-                    model: Comment,
-                    attributes: ['body']
                 }
+                // {
+                //     model: Comment,
+                //     attributes: ['body']
+                // }
             ]
         })
-        const blogs = blogData.map((blog) => {
+        const blogs = blogData.map((blog) =>
             blog.get({ plain: true })
-        })
-
+        )
+        console.log(blogs)
         res.render('homepage', {
             blogs,
             logged_in: req.session.logged_in
@@ -50,12 +50,93 @@ router.get('/signup', (req, res) => {
 })
 
 // GET if use is already logged in
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard', async (req, res) => {
     if (!req.session.logged_in) {
         res.redirect('/login')
         return
     }
-    res.render('dashboard')
+    const userData = await User.findOne({
+        where: {
+            id: req.session.user_id
+        },
+        include: [
+            {
+                model: Blog,
+                attributes: ['id', 'title', 'date', 'user_id']
+            }
+        ]
+    })
+
+    const user = userData.get({ plain: true })
+    console.log(user)
+    res.render('dashboard', {
+        ...user,
+        logged_in: req.session.logged_in
+    })
+})
+
+router.get('/blog/:id', async (req, res) => {
+    if (!req.session.logged_in) {
+        res.redirect('/login')
+        return
+    }
+
+    try {
+        const blogData = await Blog.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+                {
+                    model: Comment,
+                    attributes: ['body']
+                }
+            ]
+        })
+
+        const blog = blogData.get({ plain: true })
+
+        res.render('blog', {
+            ...blog,
+            logged_in: req.session.logged_in
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+router.get('/edit/:id', async (req, res) => {
+    if (!req.session.logged_in) {
+        res.redirect('/login')
+        return
+    }
+
+    try {
+        const blogData = await Blog.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                }
+            ]
+        })
+
+        const blog = blogData.get({ plain: true })
+console.log(blog)
+        res.render('edit', {
+            ...blog,
+            logged_in: req.session.logged_in
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
 })
 
 module.exports = router
