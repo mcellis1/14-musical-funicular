da643b9c81b92e52b3e9cd93730d9388a091932c
napling <matthewcellis98@gmail.com>
2024-12-14 12:40:54 -0600

    added post form

---
+++ public/js/post.js
@@ -0,0 +1,24 @@
+const postForm = document.querySelector('#post-form')
+
+const postFormHandler = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('#title-input').value.trim();
+    const body = document.querySelector('#body-input').value.trim();
+
+    if (title && body) {
+        const response = await fetch('/api/blogs', {
+            method: 'POST',
+            body: JSON.stringify({ title, body }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('failed to post blog');
+        }
+    }
+};
+
+postForm.addEventListener('submit', postFormHandler);
