ac73ec9801505ceeaf4c52f2c01e18fb480f96af
napling <matthewcellis98@gmail.com>
2024-12-14 23:34:11 -0600

    formatting and removed dead objects

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { Blog, User, Comment } = require('../../models')
+const { Blog } = require('../../models')
 const withAuth = require("../../utils/auth")
 
 // POST new blog
